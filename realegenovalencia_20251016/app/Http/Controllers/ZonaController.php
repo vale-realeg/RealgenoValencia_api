a38bcfb899539c6bfa0e621a649f557583ce4397
Valeria Realegeno <ia.valeria_realegeno@ufg.edu.sv>
2025-10-21 20:12:58 -0600

    Practica 21 de octubre

--- realegenovalencia_20251016/app/Http/Controllers/ZonaController.php
+++ realegenovalencia_20251016/app/Http/Controllers/ZonaController.php
@@ -1,37 +1,114 @@
 <?php
-
+//Vale OwO
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Zona;
 
 class ZonaController extends Controller
 {
     //
     public function obtenerZonas(){
         $Zona = new Zona();
+
+
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores = [];
+
         $valores = $Zona::all();
+
+        //VERIFICACION DE EXISTENCIA DE DATOS//
+        if(!empty($valores)){
+            //Si se encuentran Datos
+            $satisfactorio = true;
+            $estado = 200;
+            $mensaje = "Valores Encontrados";
+            $errores = [
+                "code" => 200,
+                "msg" => ""
+            ];
+        }
+        else{
+            //No se encuentran Datos
+            $satisfactorio = false;
+            $estado = 404;
+            $mensaje = "No se Encontraron Valores";
+            $errores = [
+                "code" => 404,
+                "msg" => "Datos No Encontrados"
+            ];
+        }
+
+        //VARIABLE DE SALIDA
         $respuesta = [
-            "success"=> true,
-            "msg" => "Valores devueltos por el EndPoint",
+            "success"=> $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
             "data" => $valores,
-            "error"=> "",
+            "errors"=> $errores,
             "total" => sizeof($valores)
         ];
-
-        return response()->json($respuesta,200);
+        //SE RETORNA EL MENSAJE AL USUARIO
+        return response()->json($respuesta,$estado);
     }
 
-    public function obtenerZona($idzona){
-        $Zona = new Zona();
-        $valores = $Zona->where('id_zona',$idzona)->get();
+    public function obtenerZona(int $idzona = 0){
+        
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores = [];
+
+        if($idzona > 0){
+            $Zona = new Zona();
+            $valores = $Zona->where('id_zona',$idzona)->get();
+
+                //VERIFICACION DE EXISTENCIA DE DATOS//
+            if(!empty($valores)){
+                //Si se encuentran Datos
+                $satisfactorio = true;
+                $estado = 200;
+                $mensaje = "Valores Encontrados";
+                $errores = [
+                    "code" => 200,
+                    "msg" => ""
+                ];
+            }
+            else{
+                //No se encuentran Datos
+                $satisfactorio = false;
+                $estado = 404;
+                $mensaje = "No se Encontraron Valores";
+                $errores = [
+                    "code" => 404,
+                    "msg" => "Datos No Encontrados"
+                ];
+            }
+        }else{
+            //No se ha enviado un valor para el parametro $idzona
+            $satisfactorio = false;
+            $estado = 400;
+            $mensaje = "No se ha enviado el Parametro Obligatorio";
+            $errores = [
+                "code" => 400,
+                "msg" => "El identificador de la Zona esta vacio"
+            ];
+        }
+
+        //Variable de Salida
         $respuesta = [
-            "success"=> true,
-            "msg" => "Valores devueltos por el EndPoint",
+            "success"=> $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
             "data" => $valores,
-            "error"=> "",
+            "errors"=> $errores,
             "total" => sizeof($valores)
         ];
 
-        return response()->json($respuesta,200);
+        return response()->json($respuesta,$estado);
     }
 }
--- realegenovalencia_20251016/routes/api.php
+++ realegenovalencia_20251016/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\ZonaController;
+use App\Http\Controllers\ZonaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
