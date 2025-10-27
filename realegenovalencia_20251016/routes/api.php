4bd78dd79c4214321105c584a3c0bb3112b6ae5b
vale-realeg <ia.valeria_realegeno@ufg.edu.sv>
2025-10-26 23:31:52 -0600

    Agregando las rutas a api.php

--- realegenovalencia_20251016/routes/api.php
+++ realegenovalencia_20251016/routes/api.php
@@ -3,11 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZonaController;
-
+//Para registro
+use App\Http\Controllers\AuthController;
+//Rutas
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/nuevousuario', [AuthController::class, 'crearUsuario']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/usuario', [AuthController::class, 'obtenerUsuario']);
+});
+
+//Cosas de clase pasada
 Route::get('/zonas',[ZonaController::class,'obtenerZonas']);
 Route::get('/zona/{idzona}',[ZonaController::class,'obtenerZona']);
 Route::get('/zonaspais/{idpais}',[ZonaController::class,'obtenerZonaPais']);
